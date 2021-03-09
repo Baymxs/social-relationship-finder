9489d8e83a06118445dd3cadd3597c7af3b3a585
n.bayramov@brain4net.com <BBoringPassWord990710b>
2021-03-09 16:48:49 +0700

    Initial commit

---
+++ src/main/java/ru/nsu/bayramov/socialrelationshipfinder/SocialrelationshipfinderApplication.java
@@ -0,0 +1,13 @@
+package ru.nsu.bayramov.socialrelationshipfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialrelationshipfinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialrelationshipfinderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/nsu/bayramov/socialrelationshipfinder/SocialrelationshipfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nsu.bayramov.socialrelationshipfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialrelationshipfinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
